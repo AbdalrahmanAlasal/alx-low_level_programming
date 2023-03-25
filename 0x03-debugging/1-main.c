fa56975757faeed166ffffa7d451ee9f73e18d85
AbdalrahmanAlasal <abdo.hassanmohamed741@gmail.com>
2023-03-24 17:21:01 -0700

    my first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,9 +2,6 @@
 
 /**
 * main - causes an infinite loop
-*
-* Describtion: avoid an infinite loop
-*
 * Return: 0
 */
 
@@ -13,15 +10,11 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-
 	/*while (i > 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
